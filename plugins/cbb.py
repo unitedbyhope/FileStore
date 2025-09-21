75f2a40d47fe0e582c8122b93a267c5ba5e67e8a
unitedbyhope <goyalmayanksingh@gmail.com>
2025-09-21 15:48:58 +0530

    Update cbb.py
    Trying to fix mention error

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -39,7 +39,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
 
     elif data == "start":
         await query.message.edit_text(
-            text=START_MSG.format(first=query.from_user.first_name),
+            text=START_MSG.format(first=query.from_user.first_name, mention=query.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
