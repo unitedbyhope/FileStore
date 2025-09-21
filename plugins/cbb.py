2ed156d7cdcd54db961037d12f82931e2e83d06c
unitedbyhope <goyalmayanksingh@gmail.com>
2025-09-21 15:18:58 +0530

    Update cbb.py
    Trial and error

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -39,7 +39,13 @@ async def cb_handler(client: Bot, query: CallbackQuery):
 
     elif data == "start":
         await query.message.edit_text(
-            text=START_MSG.format(first=query.from_user.first_name),
+            text=START_MSG.format(
+                first=message.from_user.first_name,
+                last=message.from_user.last_name,
+                username=None if not message.from_user.username else '@' + message.from_user.username,
+                mention=message.from_user.mention,
+                id=message.from_user.id
+            ),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
